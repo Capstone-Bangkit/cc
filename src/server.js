96cc6257ce61acde887686b958c25aaf40f8d8f5
yudha34 <anggoroyudap34@gmail.com>
2023-06-11 16:50:07 +0700

    update port and main

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 
 const express = require('express')
 const app = express()
-const port = 3001
 const bodyParser = require('body-parser')
 const connection = require('./connection')
 const response = require('./response')
 
 app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+ 
+
+    response(200, "Response Success", "Api-Started", res)
+    console.log("Response success")
+
+
+})
+
 
 app.get('/equipment', (req, res) => {
   const sql = "SELECT * FROM  equipment"
@@ -26,6 +35,7 @@ app.get('/equipment/:equipment_id', (req, res) => {
   
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log("App listening on Port " + PORT)
 })
