9ce0840bd62f17bc25b1324de9eb02deda1e6313
yudha34 <anggoroyudap34@gmail.com>
2023-06-11 19:36:41 +0700

    ready to deploy

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.get('/equipment/:equipment_id', (req, res) => {
   const sql = `SELECT * FROM equipment WHERE equipment_id = ${req.params.equipment_id} `
   connection.query(sql, (err, result) => {
     //hasil dari query mysql
-    response(200, result, "get all data from equipment", res)
+    response(200, result, `get data by id ${req.params.equipment_id}`, res)
   })
   
 })
